5fcc5a39310425ba07b0f6fe887887b4c8fad0b9
LAPTOP-KKQ8B52S\Ruben Frans <rubenfrans0011@hotmail.com>
2022-05-28 15:52:15 +0200

    Implements file writing to custom path and fixes argument checking

--- TextConverterCommandLineTool/main.cpp
+++ TextConverterCommandLineTool/main.cpp
@@ -13,112 +13,123 @@
 
 struct ConverterSettings {
 
-    std::string inputFile{};
-    std::string outputFile{};
-    bool utf8ToCodePage{ true };
+	std::string inputFile{};
+	std::string outputFile{};
+	bool utf8ToCodePage{ true };
 };
 
-bool ParseThreeArguments(char* argv[], ConverterSettings& args);
+bool ParseArguments(int argc, char* argv[], ConverterSettings& args);
 
 void PrintHelp();
 void PrintError(const std::string& errorMsg);
 
 int main(int argc, char* argv[]) {
 
-    if (argc < 3) {
-        std::cout << "Not enough arguments" << std::endl;
-        PrintHelp();
-        return 1;
-    }
+	if (argc < 3) {
+		std::cout << "Not enough arguments" << std::endl;
+		PrintHelp();
+		return 1;
+	}
 
-    ConverterSettings settings{};
+	ConverterSettings settings{};
 
-    if (argc == 3) {
 
-        bool result = ParseThreeArguments(argv, settings);
+	bool result = ParseArguments(argc, argv, settings);
 
-        if (!result) {
-            return 1;
-        }
-    }
+	if (!result) {
+		return 1;
+	}
 
-    TextConverter converter{};
 
-    if (settings.utf8ToCodePage) {
-     
-        if (settings.outputFile.empty()) {
-            converter.ConvertUTF8FileToCodePage(settings.inputFile);
-        }
-        else {
-            converter.ConvertUTF8FileToCodePage(settings.inputFile, settings.outputFile);
-        }
-    }
-    else {
+	TextConverter converter{};
 
-        if (settings.outputFile.empty()) {
-            converter.ConvertCodePageToUTF8(settings.inputFile);
-        }
-        else {
-            converter.ConvertCodePageToUTF8(settings.inputFile, settings.outputFile);
-        }
-    }
+	if (settings.utf8ToCodePage) {
 
-    return 0;
+		if (settings.outputFile.empty()) {
+			converter.ConvertUTF8FileToCodePage(settings.inputFile);
+		}
+		else {
+			converter.ConvertUTF8FileToCodePage(settings.inputFile, settings.outputFile);
+		}
+	}
+	else {
+
+		if (settings.outputFile.empty()) {
+			converter.ConvertCodePageToUTF8(settings.inputFile);
+		}
+		else {
+			converter.ConvertCodePageToUTF8(settings.inputFile, settings.outputFile);
+		}
+	}
+
+	return 0;
 }
 
 // The correct syntax of 3 arguments is >> TextConverterCommandlineTool.exe /u <inputfile string>
-bool ParseThreeArguments(char* argv[], ConverterSettings& settings) {
-    
-    std::string arg1{ argv[1] };
-    std::string arg2{ argv[2] };
+bool ParseArguments(int argc, char* argv[], ConverterSettings& settings) {
 
-    if (std::isdigit(*argv[1])) {
-        PrintError("First argument cannot be a number");
-        PrintHelp();
-        return false;
-    }
+	std::string arg1{ argv[1] };
+	std::string arg2{ argv[2] };
 
-    if (arg1.compare("u")) {
-        settings.utf8ToCodePage = true;
-    }
-    else if (arg1.compare("c")) {
-        settings.utf8ToCodePage = false;
-    }
-    else {
-        PrintError("ERROR: First argument should be either \"u\" or \"c\"");
-        PrintHelp();
-        return false;
-    }
+	std::string arg3{};
 
-    std::filesystem::path inputFilePath{arg2};
+	if (argc >= 4) {
+		arg3 = std::string{ argv[3] };
+	}
 
-    bool result = std::filesystem::exists(inputFilePath);
 
-    if (result) {
-        settings.inputFile = arg2;
-    }
-    else {
-        PrintError("inputFile does not exists");
-        return false;
-    }
+	if (std::isdigit(*argv[1])) {
+		PrintError("First argument cannot be a number");
+		PrintHelp();
+		return false;
+	}
 
-    return true;
+	if (arg1.compare("u") == 0) {
+		settings.utf8ToCodePage = true;
+	}
+	else if (arg1.compare("c") == 0) {
+		settings.utf8ToCodePage = false;
+	} else {
+		PrintError("ERROR: First argument should be either \"u\" or \"c\"");
+		PrintHelp();
+		return false;
+	}
+
+	std::filesystem::path inputFilePath{ arg2 };
+
+	bool result = std::filesystem::exists(inputFilePath);
+
+	if (result) {
+		settings.inputFile = arg2;
+	}
+	else {
+		PrintError("inputFile does not exists");
+		return false;
+	}
+
+
+	if (arg3.empty())
+		return true;
+
+	settings.outputFile = arg3;
+
+	return true;
 }
 
 void PrintHelp() {
 
-    std::cout << "TextConverter Command line help" << std::endl;
-    std::cout << "===============================" << std::endl;
+	std::cout << "TextConverter Command line help" << std::endl;
+	std::cout << "===============================" << std::endl;
 
-    std::cout << "Usage: " << std::endl;
-    std::cout << "TextConverterCommandLineTool.exe <flag> <inputfile> <outputfile> " << std::endl;
-    std::cout << "<flag> options:" << std::endl;
-    std::cout << "\tu: convert Code Page 437 file to UTF-8" << std::endl;
-    std::cout << "\tc: convert UTF-8 to Code Page 437" << std::endl;
+	std::cout << "Usage: " << std::endl;
+	std::cout << "TextConverterCommandLineTool.exe <flag> <inputfile> <outputfile> " << std::endl;
+	std::cout << "<flag> options:" << std::endl;
+	std::cout << "\tu: convert UTF-8 to Code Page 437" << std::endl;
+	std::cout << "\tc: convert Code Page 437 to UTF-8" << std::endl;
 }
 
 void PrintError(const std::string& errorMsg) {
 
-    std::cout << "Error: " << errorMsg << std::endl;
+	std::cout << "Error: " << errorMsg << std::endl;
 
 }
